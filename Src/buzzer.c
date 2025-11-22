aae0324c59e0c68483b99b1d71f0c33fc7e756df
danielarci4 <ds.arcilag@gmail.com>
2025-11-22 18:51:31 -0500

    Errors related to PWM were corrected.

--- Src/buzzer.c
+++ Src/buzzer.c
@@ -4,11 +4,12 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-#define SM 1                 ///< State machine index    
-#define PIO pio0             ///< PIO instance
-#define OUTPUT_DIR 1         ///< Set pin direction to output
-#define BUZZER_PIN 15        ///< Pin connected to the buzzer
-#define SYS_CLK 125000000.0f ///< System clock frequency in Hz
+#define SM 1                         ///< State machine index    
+#define PIO pio0                     ///< PIO instance
+#define OUTPUT_DIR 1                 ///< Set pin direction to output
+#define BUZZER_PIN 15                ///< Pin connected to the buzzer
+#define SYS_CLK 125000000.0f         ///< System clock frequency in Hz
+#define SILENCE_BETWEEN_NOTES_MS 100 ///< Silence duration between notes in milliseconds
 
 volatile bool is_playing = false;  ///< Flag to indicate if the song has finished
 volatile uint offset;              ///< Offset of the PIO program in the PIO instruction memory
@@ -27,7 +28,7 @@ typedef struct {
 
 /*************************************STRUCTS SONG/NOTES SECTION********************************************/
 const note_t beep_notes[] = {
-    {440.0f, 100}
+    {3000.0f, 200}
 };
 
 song_t beep_song = {
@@ -37,8 +38,8 @@ song_t beep_song = {
 };
 
 const note_t correct_password_notes[] = {
-    {220.0f, 500},
-    {440.0f, 500}
+    {2200.0f, 500},
+    {4400.0f, 1000}
 };
 
 song_t correct_password_song = {
@@ -48,7 +49,7 @@ song_t correct_password_song = {
 };
 
 const note_t incorrect_password_notes[] = {
-    {220.0f, 1000}
+    {2200.0f, 1000}
 };
 
 song_t incorrect_password_song = {
@@ -58,60 +59,77 @@ song_t incorrect_password_song = {
 };
 /******************************************************************************************************/
 
-static bool duration_timer_callback(repeating_timer_t *rt);  ///< Timer callback for debouncing
-static repeating_timer_t duration_timer;                     ///< Timer for duration control
-
-// Convert frequency to PIO cycles
-uint32_t cycles_from_freq(float freq) {
-    return (uint32_t)(SYS_CLK / (freq * 2));
+// Write `period` to the input shift register
+void pio_pwm_set_period(uint32_t period) {
+    pio_sm_put_blocking(PIO, SM, period);
+    pio_sm_exec(PIO, SM, pio_encode_pull(false, false));
+    pio_sm_exec(PIO, SM, pio_encode_out(pio_isr, 32));
 }
+
+// Write `level` to TX FIFO. State machine will copy this into X.
+void pio_pwm_set_level(uint32_t level) {
+    pio_sm_put_blocking(PIO, SM, level);
+}
+
+uint32_t freq_to_cicles(float frequency) {
+    return (uint32_t)(SYS_CLK / (frequency*2));
+}
+/******************************************************************************************************/
+static bool finished_note_timer_callback(repeating_timer_t *rt);  ///< Timer callback for duration control
+static bool silence_timer_callback(repeating_timer_t *rt);   ///< Timer callback for silence between notes
+static repeating_timer_t duration_timer;                     ///< Timer for duration control
+static repeating_timer_t silence_timer;                      ///< Use a separate timer for silence
 
 // Play a single note
-void play_note(note_t note) {
-    uint32_t cycles = cycles_from_freq(note.frequency);
+void play_note(const note_t * note) {
+    uint32_t cicles = freq_to_cicles((* note).frequency);
 
-    pio_sm_exec(PIO, SM, pio_encode_jmp(offset));  // salta al inicio del programa
-    pio_sm_put_blocking(PIO, SM, cycles);
-    add_repeating_timer_ms(note.duration_ms, duration_timer_callback, NULL, &duration_timer);
+    pio_pwm_set_period(cicles);                    ///< Set the period based on frequency
+    pio_pwm_set_level(cicles/2);                   ///< Set duty cycle to 50%
+    pio_sm_exec(PIO, SM, pio_encode_jmp(offset));  ///< jump to start of program
+    pio_sm_set_enabled(PIO, SM, true);             ///< Re-Enable the state machine
+    add_repeating_timer_ms((* note).duration_ms, finished_note_timer_callback, NULL, &duration_timer); ///< Set timer for note duration
 }
 
-// Timer callback to handle note duration
-static bool duration_timer_callback(repeating_timer_t *rt) {
-    beep_song.current_note_index++;                         // Advance to the next index note
-
+static bool silence_timer_callback(repeating_timer_t *rt) {
+    // Advance to the next index note
+    beep_song.current_note_index++; 
     // Check if the song has finished
     if (beep_song.current_note_index >= (beep_song.length)) {
         // Reset song state
         beep_song.current_note_index = 0;
         is_playing = false;
-        pio_sm_set_enabled(PIO, SM, false);                  // Disable the PIO state machine
-        pio_sm_exec(PIO, SM, pio_encode_set(pio_pins, 0));   // Turn off the buzzer
-        pio_sm_drain_tx_fifo(PIO, SM);                       // Clear TX FIFO
         return false;
     }
-    play_note(beep_song.notes[beep_song.current_note_index]); // Play the next note
+    // Move to the next note
+    const note_t * next_note  = &beep_song.notes[beep_song.current_note_index];
+    play_note(next_note); // Play the next note
+    return false;
+}
+
+// Timer callback to finish note 
+static bool finished_note_timer_callback(repeating_timer_t *rt) {
+    pio_sm_set_enabled(PIO, SM, false);
+    pio_sm_exec(PIO, SM, pio_encode_sideset_opt(1,0));          // Turn off the buzzer
+    // pio_sm_drain_tx_fifo(PIO, SM);                       // Clear TX FIFO
+    add_repeating_timer_ms(SILENCE_BETWEEN_NOTES_MS, silence_timer_callback, NULL, &silence_timer);
     return false;
 }
 
 // Initialize the buzzer PIO and state machine
 void buzzer_init_pio() {
-    pio_gpio_init(PIO, BUZZER_PIN);
     offset = pio_add_program(PIO, &buzzer_square_program);
-    pio_sm_config c = buzzer_square_program_get_default_config(offset);
-    pio_sm_set_consecutive_pindirs(PIO, SM, BUZZER_PIN, 1, OUTPUT_DIR);
-    sm_config_set_set_pins(&c, BUZZER_PIN, 1);
-    sm_config_set_in_shift(&c, 0, false, 0);
-    
-    pio_sm_init(PIO, SM, offset, &c);
+    pwm_program_init(PIO, SM, offset, BUZZER_PIN);    
     pio_sm_set_enabled(PIO, SM, false);
 }
 
 // Play melody based on the type
 void play_melody(char melody_type) {
-    if(!is_playing){
+    if(!is_playing){                  /// Only play if no song is currently playing
         song_t * current_song;        ///< Current song being played
         const note_t * first_note;    ///< Pointer to the first note of the song
         is_playing = true;            ///< Set the playing flag to true
+
         if(melody_type == 'B'){
             current_song = &beep_song;               ///< Set current song to beep song
             first_note = &(* current_song).notes[beep_song.current_note_index];   /// Get the first note
@@ -121,13 +139,10 @@ void play_melody(char melody_type) {
         } else if(melody_type == 'I'){
             current_song = &incorrect_password_song; ///< Set current song to correct password song
             first_note = &(* current_song).notes[incorrect_password_song.current_note_index];
-            printf("incorrect password melody\n");
         } else {
             is_playing = false;       ///< Reset playing flag if no song is played
             return;
         }
-      
-        pio_sm_set_enabled(PIO, SM, true); ///< Enable the PIO state machine
-        play_note(*first_note);            ///< Play the first note
+        play_note(first_note);            ///< Play the first note
     } 
 }
--- Src/matrix_keyboard.c
+++ Src/matrix_keyboard.c
@@ -9,13 +9,14 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-#define PIO pio0              ///< PIO instance
 #define SM 0                  ///< State machine index
-#define BASE_OUT 2            ///< Starting pin for output pins
+#define PIO pio0              ///< PIO instance
 #define BASE_IN 6             ///< Starting pin for input pins
+#define BASE_OUT 2            ///< Starting pin for output pins
+#define PIN_RELE 22           ///< Pin number for the relay control
 #define OUTPUT_DIR 1          ///< Set pin direction to output
-#define MAX_PASSWORD_LENGTH 4 ///< Maximum length of the password
 #define DEBOUNCE_DELAY  250   ///< Debounce delay in milliseconds
+#define MAX_PASSWORD_LENGTH 4 ///< Maximum length of the password
 
 typedef struct {
     uint8_t row;
@@ -26,6 +27,9 @@ typedef struct {
 static bool key_timer_callback(repeating_timer_t *rt);  ///< Timer callback for debouncing
 static repeating_timer_t debounce_timer;                ///< Timer for debouncing the button press
 
+static bool rele_timer_callback(repeating_timer_t *rt);  ///< Timer callback for debouncing
+static repeating_timer_t on_rele_timer;                ///< Timer for debouncing the button press
+
 // Interrupt Service Routine for keyboard PIO where key press is detected
 static void keyboard_isr_pio(void)                     
 {
@@ -35,6 +39,7 @@ static void keyboard_isr_pio(void)
     pio_interrupt_clear(PIO, 0);
 }
 
+// Function to validate the entered password
 bool validate_password(const char *input_password)
 {
     for(uint8_t i = 0; i < MAX_PASSWORD_LENGTH; i++) {
@@ -43,8 +48,10 @@ bool validate_password(const char *input_password)
     return true;
 }
 
+// Function to decode the key position from the key value
 void decode_key(key_position_t * key)
 {
+    // Decode for last 4 bits 
     if(key->key_value & 0xF000){
         key->row = 0;
         if((key->key_value >> 12) & 0x01){
@@ -56,6 +63,7 @@ void decode_key(key_position_t * key)
         } else if((key->key_value >> 12) & 0x08){
             key->col = 3;
         }
+        // Decode for next 4 bits
     } else if (key->key_value & 0x0F00){
         key->row = 1;
         if((key->key_value >> 8) & 0x01){
@@ -67,6 +75,7 @@ void decode_key(key_position_t * key)
         } else if((key->key_value >> 8) & 0x08){
             key->col = 3;
         }
+        // Decode for next 4 bits
     } else if (key->key_value & 0x00F0){
         key->row = 2;
         if((key->key_value >> 4) & 0x01){
@@ -78,6 +87,7 @@ void decode_key(key_position_t * key)
         } else if((key->key_value >> 4) & 0x08){
             key->col = 3;
         }
+        // Decode for fisrt 4 bits
     }else if (key->key_value & 0x000F){
         key->row = 3;
         if((key->key_value >> 0) & 0x01){
@@ -115,7 +125,14 @@ static inline void detection_keypad(uint32_t key_value, uint8_t *current_positio
         play_melody('B'); // Play beep melody on key press
     }else {
         // Play melody based on password validity
-        validate_password((const char *) password) ?  play_melody('C') :  play_melody('I'); 
+        if(validate_password((const char *) password)) {
+            printf("Password correct!\n");
+            play_melody('C'); // Correct password melody
+            gpio_put(PIN_RELE, 1); // Activate relay
+            add_repeating_timer_ms(2000, rele_timer_callback, NULL, &on_rele_timer); // Deactivate relay after 2 seconds
+        } else {
+            play_melody('I'); // Incorrect password melody
+        }
         password[*current_position] = '\0'; // Null-terminate the string
         *current_position = 0;              // Reset for next input
     }
@@ -128,12 +145,19 @@ static bool key_timer_callback(repeating_timer_t *rt) {
             uint32_t key = pio_sm_get(pio0, 0);
             static uint8_t current_position = 0;
             static char password [ MAX_PASSWORD_LENGTH ];
+            // Handle key press and update password
             detection_keypad(key, &current_position, password);
         }
         pio_sm_set_enabled(PIO, SM, true); // Re-enable the state machine
         return false;  // stop timer
     }
     return true;  // hold timer
+}
+
+bool rele_timer_callback(repeating_timer_t *rt)
+{
+    gpio_put(PIN_RELE, 0); // Deactivate relay
+    return false;
 }
 
 // Initialize the matrix keyboard PIO
@@ -168,6 +192,13 @@ void matrix_keyboard_init_pio(void)
     // enable the sm
     pio_sm_set_enabled(PIO, SM, true);
     buzzer_init_pio();
+
+    // Initialize relay control pin
+    gpio_init(PIN_RELE);
+    gpio_set_dir(PIN_RELE, GPIO_OUT);
+    // put pull-down to ensure relay is off initially
+    gpio_pull_down(PIN_RELE);
+    gpio_put(PIN_RELE, 0); // Ensure relay is off initially
 }
 
 
